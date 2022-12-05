84ce1ec9f802affe1f46fd8b537b9465b2f1a647
koallider <xehxtkf@gmail.com>
2022-12-05 10:55:08 +0300

    example

---
+++ example/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:image_gradient/image_gradient.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<Gradient> exampleGradients;
+  int activeGradient = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    exampleGradients = [
+      const RadialGradient(colors: [Colors.blue, Colors.cyanAccent]),
+      const RadialGradient(colors: [Colors.deepOrange, Colors.purpleAccent]),
+      const RadialGradient(colors: [Colors.yellow, Colors.pinkAccent]),
+      const LinearGradient(colors: [Colors.yellow, Colors.pinkAccent]),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children:
+                  List.generate(exampleGradients.length, buildGradientButton),
+            ),
+            Expanded(child: ImageGradient(
+              image: Image.asset("assets/light.png"),
+              gradient: exampleGradients[activeGradient],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildGradientButton(int index) {
+    return InkWell(
+        onTap: () {
+          setState(() {
+            activeGradient = index;
+          });
+        },
+        child: Container(
+          width: 50,
+          height: 50,
+          margin: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              gradient: exampleGradients[index],
+              border: Border.all(color: Colors.grey)),
+        )
+    );
+  }
+}
