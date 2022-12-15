04f845cd1a924ccc4d306a8a58f58a9305ef5020
Koallider <xehxtkf@gmail.com>
2022-12-15 08:26:16 +0300

    Alternative constructors

--- lib/image_gradient.dart
+++ lib/image_gradient.dart
@@ -1,5 +1,7 @@
 library image_gradient;
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 /// A widget to apply gradient to image.
@@ -11,6 +13,36 @@ class ImageGradient extends StatelessWidget {
   final Gradient gradient;
 
   const ImageGradient({super.key, required this.image, required this.gradient});
+
+  ImageGradient.linear({
+    super.key,
+    required this.image,
+    required List<Color> colors,
+    Alignment begin = Alignment.centerLeft,
+    Alignment end = Alignment.centerRight,
+  }) : gradient = LinearGradient(colors: colors, begin: begin, end: end);
+
+  ImageGradient.radial(
+      {super.key,
+      required this.image,
+      required List<Color> colors,
+      Alignment center = Alignment.center,
+      double radius = 0.5})
+      : gradient =
+            RadialGradient(colors: colors, center: center, radius: radius);
+
+  ImageGradient.sweep({
+    super.key,
+    required this.image,
+    required List<Color> colors,
+    Alignment center = Alignment.center,
+    double startAngle = 0.0,
+    double endAngle = pi * 2,
+  }) : gradient = SweepGradient(
+            colors: colors,
+            center: center,
+            startAngle: startAngle,
+            endAngle: endAngle);
 
   @override
   Widget build(BuildContext context) {
